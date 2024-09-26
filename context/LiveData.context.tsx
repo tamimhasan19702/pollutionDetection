318833f7a2ba190d81f2e0a44b1ba88871a25b82
tamimhasan19702 <tamimhasancu@gmail.com>
2024-09-26 21:06:23 +0600

    update app/(tabs)/(location)/index.tsx and LiveData.context.tsx

--- context/LiveData.context.tsx
+++ context/LiveData.context.tsx
@@ -1,8 +1,7 @@
 /** @format */
-
 import { createContext, useState, useEffect, ReactNode } from "react";
 import { Database } from "@/firebase.config";
-import { ref, set, remove } from "firebase/database";
+import { ref, set, remove, onValue } from "firebase/database";
 
 export const LiveDataContext = createContext({
   liveData: [] as LiveDataType[],
@@ -29,17 +28,13 @@ const formatTime = (date: Date) => {
   let ampm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
-  // @ts-ignore
   minutes = minutes < 10 ? "0" + minutes : minutes;
   let strTime = hours + ":" + minutes + " " + ampm;
-
   // Format date
   let day = date.getDate();
   let month = date.getMonth() + 1; // Months are zero-based
   let year = date.getFullYear();
-
   let strDate = `${month}/${day}/${year}`;
-
   return `${strDate} - ${strTime}`;
 };
 
@@ -50,10 +45,26 @@ export const LiveDataProvider = ({ children }: LiveDataProviderProps) => {
 
   const handleDelete = (index: number) => {
     setLiveData((prevData) => prevData.filter((_, i) => i !== index));
-
     const dbRef = ref(Database, `locationData/${index}`);
     remove(dbRef);
   };
+
+  useEffect(() => {
+    const dbRef = ref(Database, "locationData");
+    onValue(dbRef, (snapshot) => {
+      const liveDataArray = snapshot.val();
+      if (liveDataArray) {
+        setLiveData(Object.values(liveDataArray));
+      } else {
+        setLiveData([]);
+      }
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   const dbRef = ref(Database, "locationData");
+  //   set(dbRef, liveData);
+  // }, [liveData]);
 
   return (
     <LiveDataContext.Provider value={{ liveData, setLiveData, handleDelete }}>
